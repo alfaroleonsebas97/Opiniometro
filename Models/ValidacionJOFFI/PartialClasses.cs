88cabe0493ca5d100b62c8ba68418919ef6fc5f0
Sebastián Alfaro León <37912510+alfaroleonsebas97@users.noreply.github.com>
2021-06-25 11:05:48 -0600

    Add files via upload

---
+++ Models/ValidacionJOFFI/PartialClasses.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Opiniometro_WebApp.Models
+{
+    [MetadataType(typeof(FormularioMetadata))]
+    public partial class Formulario
+    {
+    }
+
+    [MetadataType(typeof(SeccionMetadata))]
+    public partial class Seccion
+    {
+    }
+
+    [MetadataType(typeof(CategoriaMetadata))]
+    public partial class Categoria
+    {
+    }
+
+    [MetadataType(typeof(ItemMetadata))]
+    public partial class Item
+    {
+    }
+
+    [MetadataType(typeof(CrearFormularioMetadata))]
+    public partial class CrearFormulario
+    {
+
+    }
+
+    // The Strategists.
+    [MetadataType(typeof(PerfilMetadata))]
+    public partial class Perfil
+    {
+    }
+}
