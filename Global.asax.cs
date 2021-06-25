e2d53c581c9c9d74e064cfbeebab1f8524096d68
Sebastián Alfaro León <37912510+alfaroleonsebas97@users.noreply.github.com>
2021-06-25 11:07:22 -0600

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Opiniometro_WebApp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Para solucionar el problema con el atributo [ValidateAntiForgeryToken].
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.Email;
+        }
+    }
+}
